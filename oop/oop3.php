0ddecd94f4376578af3e046329733d16d28877e5
Hassan Nadeem <64688959+HassanNadeemWebD@users.noreply.github.com>
2023-02-07 10:53:55 +0500

    commit

---
+++ oop/oop3.php
@@ -0,0 +1,97 @@
+<?php
+
+interface CrudApp
+{
+
+    function create();
+    function read();
+    function update();
+    function delete();
+}
+abstract  class Book
+{
+
+    public $name, $pages, $author, $price;
+
+
+    function show()
+    {
+
+        echo  $this->name;
+        echo  $this->pages;
+        echo  $this->author;
+        echo  $this->price;
+    }
+
+    function __construct($name, $pages, $author, $price)
+    {
+
+        $this->name =  $name;
+        $this->pages = $pages;
+        $this->author = $author;
+        $this->price = $price;
+    }
+
+    protected abstract function abs();
+}
+
+
+
+class Horror extends Book
+{
+    function abs()
+    {
+
+        echo "Horror Book";
+    }
+}
+
+
+class User implements CrudApp
+{
+
+
+    function create()
+    {
+
+        echo "Create Record";
+    }
+    function read()
+    {
+
+        echo "Read";
+    }
+    function update()
+    {
+
+        echo "update";
+    }
+    function delete(){
+
+        echo "delete";
+    }
+
+
+function all()
+{
+   $this->create(); 
+   $this->read(); 
+   $this->update(); 
+   $this->delete(); 
+}
+
+}
+
+
+
+// $b1 = new Horror("Ajab Kahani","150","Hunain",550);
+
+// $b1->name = "Hello";
+
+// echo  $b1->name;
+
+// $b1->abs();
+
+// $b1->show();
+$u1 = new User();
+$u1->all();
